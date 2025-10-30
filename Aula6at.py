b217d51f80ef5992079e41d0ced7aab96c2f30d7
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2025-10-29 21:43:36 -0300

    Add files via upload

---
+++ Aula6at.py
@@ -0,0 +1,46 @@
+# VAMOS PRATICAR COM VARIÁVEIS:
+
+# Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+print('Resultado',4*6/2)
+# Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+print('Resultado',5+7)
+# Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+print('Resultado',3**4)
+# Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+print('Resultado',35/3)
+# Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+print('Resultado',8*5)
+# Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+print('Resultado',27/3+5)
+# Exercícios com variáveis / print() / input()(opcional):
+n1 = int(input('Digite um numero'))
+n2 = int(input('Digite um numero'))
+print(n1+n2)
+# Calcule o módulo ( % ) de 17 por 4.
+print()
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+print((9*3)**2)
+# Calcule a raiz quadrada de 81.
+quadrada = 81  ** 0.5
+print(quadrada)
+# Adicione 20 a 3 vezes 4.
+n1 = 3
+n2 = 4
+n3 = 20
+print((n1*n2)+20)
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+print((15*2)-7)
+# Eleve 5 ao cubo.
+print(5 ** 3)
+# Calcule a média de 17, 21 e 25.
+print((17+21+25)/3)
+# Multiplique 11 por 2 e adicione 7.
+print((11*2)+7)
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+n1 = 3
+n2 = 8
+n3 = 15
+n4 = 2
+print(((n1 * n2)-15)/2)
+# Eleve 2 à potência de 10.
+print(2 ** 10)
